381e3dc86d1af87c74ca4213af4c562e5023ae12
ohanlon <pete.ohanlon@gmail.com>
2019-01-14 10:13:54 +0000

    Accidentally missed out a non capturing group

--- chapter03/src/Validation/PhoneValidation.ts
+++ chapter03/src/Validation/PhoneValidation.ts
@@ -5,7 +5,7 @@ import { IValidation } from './IValidation';
 
 export class PhoneValidation implements IValidation {
 
-  private readonly regexValidator : RegularExpressionValidator = new RegularExpressionValidator(`^(?:\\(([0-9]{3})\\)|(?:[0-9]{3}))[-. ]?(?:[0-9]{3})[-. ]?(?:[0-9]{4})$`);
+  private readonly regexValidator : RegularExpressionValidator = new RegularExpressionValidator(`^(?:\\((?:[0-9]{3})\\)|(?:[0-9]{3}))[-. ]?(?:[0-9]{3})[-. ]?(?:[0-9]{4})$`);
   private readonly minLengthValidator : MinLengthValidator = new MinLengthValidator(1);
 
   public Validate(state : IPersonState, errors : string[]) : void {
